6536c4955e13fd67c939a6fc6cc687d29e976d15
auxten <auxtenwpc@gmail.com>
2012-06-25 18:17:52 +0800

    Update master

--- fhost/utils.py
+++ fhost/utils.py
@@ -47,9 +47,7 @@ def isValidHostname(hostname):
 
     :Results: Boolean value that indicates if the hostname is valid.
     """
-    regepx = """^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)
-                 *
-                 ([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$"""
+    regexp = """^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$"""
     return re.match(regexp, hostname)
 
 
@@ -68,11 +66,5 @@ def isValidIP(ip):
 
     :Results: Boolean value that indicates if the IP address is valid.
     """
-    regexp = """^(([0-9]|[1-9][0-9]|1[0-9]
-             {2}
-             |2[0-4][0-9]|25[0-5])\.)
-             {3}
-             ([0-9]|[1-9][0-9]|1[0-9]
-             {2}
-             |2[0-4][0-9]|25[0-5])$"""
+    regexp = """^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"""
     return re.match(regexp, ip)
